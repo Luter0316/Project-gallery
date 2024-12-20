2004675d033b937a05ea17b3942b9c4afcd0d8a1
Vitaliy <vitaliy0316@mail.ru>
2024-12-20 10:33:07 +0300

    Создана модель проектов, директория для хранения фотографий

--- backend/gallery/models.py
+++ backend/gallery/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='static/gallery/img', blank=True) # blank=True - не обязательное поле
+
+    def __str__(self):
+        return self.title
